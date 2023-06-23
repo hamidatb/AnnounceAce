ccca464cf85dcf7a4af1e64ee0b4cd6325a75c78
Hamidat Bello <107907514+hamidatb@users.noreply.github.com>
2023-06-23 01:12:45 +0000

    Changed the structure to use bot commands.

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,54 +1,30 @@
 import os
 from dotenv import load_dotenv
 import discord
-import responses
+from discord.ext import commands
 
+load_dotenv()
+token = os.getenv('TOKEN')
+intents = discord.Intents.default()
+intents.messages = True
+intents.message_content = True
 
-def run_discord_bot():
-    load_dotenv()
-    token = os.getenv('TOKEN')
-    intents = discord.Intents.default()
-    intents.messages = True  # Enable messages
-    intents.message_content = True  # Enable message content
-    client = discord.Client(intents=intents)  # Pass intents as a keyword argument
+bot = commands.Bot(command_prefix="!", intents=intents)  # Using "!" as the command prefix.
 
-    @client.event
-    async def on_ready():
-        print("AnnounceAce has logged in as {0.user}".format(client))
+@bot.event
+async def on_ready():
+    print(f"{bot.user} has connected to Discord!")
 
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
-            return
-        
-        username = str(message.author)
-        user_message = str(message.content)
-        channel = str(message.channel)
-        
-        print(f"{username} said: '{user_message}' ({channel})")
-        
-        # If the message starts with a ?, respond to the user in private
-        if user_message.startswith("?"):
-            user_message = user_message[1:]
-            await send_response(message, user_message, is_private=True)
+@bot.command(name='hello')
+async def hello(ctx):
+    await ctx.send("Hello!")
 
-        # If the message starts with a &, respond to the user in the public channel in which the message was sent.
-        elif user_message.startswith("&"):
-            user_message = user_message[1:]
-            await send_response(message, user_message, is_private=False)
+@bot.command(name='private')
+async def private_message(ctx, *, message):
+    await ctx.author.send(message)
 
-        #If the message does not start with either & or ?, AnnounceAce will ignore it.
-    client.run(token)
+@bot.command(name='public')
+async def public_message(ctx, *, message):
+    await ctx.send(message)
 
-
-async def send_response(message, user_message, is_private):
-    try:
-        response = responses.get_responses(user_message)
-        # Send the message directly to the author if it is a private message, otherwise send publicly in the channel.
-        await message.author.send(response) if is_private else await message.channel.send(response)
-    except Exception as error:
-        print(error)
-
-
-if __name__ == '__main__':
-    run_discord_bot()
+bot.run(token)
