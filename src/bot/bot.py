33415b7293167cbb4209823439034ed27ea4d2c7
Hamidat Bello <107907514+hamidatb@users.noreply.github.com>
2023-06-23 00:00:04 +0000

    Moving the location of the bot file.

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -3,13 +3,14 @@ from dotenv import load_dotenv
 import discord
 import responses
 
-# load values from .env file
-load_dotenv()
-token = os.getenv('TOKEN')
 
 def run_discord_bot():
+    load_dotenv()
+    token = os.getenv('TOKEN')
     intents = discord.Intents.default()
-    intents.message_content = Trueclient = discord.Client(intents)
+    intents.message_content = True
+    client = discord.Client(intents)
+    client.run(token)
 
     @client.event
     #This triggers everytime this code is ran, and lets you know that the bot is ready to be used on the server.
@@ -20,7 +21,19 @@ def run_discord_bot():
     async def on_message(message):
         if message.author == client.user:
             return
+        
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
 
+        print(f"{username} said: '{user_message}" ({channel}))
+        
+        #This means it removes the question mark at the start of the message sent in private so that it processes the string normally.
+        if user_message [0] == "?":
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
 
 
 async def send_message(message, user_message, is_private):
