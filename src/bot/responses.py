27b58e54a932f3e963941f3e47a8b21aae56ad14
Hamidat Bello <107907514+hamidatb@users.noreply.github.com>
2023-06-23 00:46:36 +0000

    Updating.

--- src/bot/responses.py
+++ src/bot/responses.py
@@ -1,14 +1,9 @@
-import random
-
 # Get responses will take in a string and then return a string.
 def get_responses(message: str) -> str:
     p_message = message.lower()
 
     if p_message == 'hello':
         return "Hey there!"
-    
-    if message == "roll":
-        return str(random.randint(1,6))
     
     if p_message == "!help":
         return "`This is a help message that you can modify.`"
